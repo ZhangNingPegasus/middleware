bd64494797169010262c28d6bda17afafbd41cef
pegasus <349409664@qq.com>
2021-01-26 19:26:35 +0800

    修改StringUtils

--- redis-starter/src/main/java/org/wyyt/redis/aop/DistributedLockAop.java
+++ redis-starter/src/main/java/org/wyyt/redis/aop/DistributedLockAop.java
@@ -1,6 +1,6 @@
 package org.wyyt.redis.aop;
 
-import com.sun.deploy.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
